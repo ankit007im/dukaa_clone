ec7f33a474ae7a5586697ee6b2e45170c0ce1dd0
ankit007im <ankit1141122@gmail.com>
2020-12-11 21:27:45 +0530

    Improved roject structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-import 'package:dukaan_clone/components/counter/counter_example.dart';
-import 'package:dukaan_clone/components/todo/todo_example.dart';
+import 'package:dukaan_clone/pages/SplashScreen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -10,66 +9,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
         home: Scaffold(
-          appBar: AppBar(
-            title: Text("Examples of mobX"),
-          ),
-          body: ExamplePages(),
+          body: SplashScreen(),
         ));
-  }
-}
-
-class ExamplePages extends StatefulWidget {
-  @override
-  _ExamplePagesState createState() => _ExamplePagesState();
-}
-
-class _ExamplePagesState extends State<ExamplePages> {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          MaterialButton(
-              color: Colors.deepOrange,
-              child: Text("Counter Page"),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => CounterExample(
-                            title: "Counter App",
-                          )),
-                );
-              }),
-          MaterialButton(
-              color: Colors.deepOrange,
-              child: Text("Todo App"),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => TodoApp(title: "Todo App")),
-                );
-              }),
-          MaterialButton(
-              color: Colors.deepOrange,
-              child: Text("Page 3"),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => CounterExample(
-                            title: "Counter App",
-                          )),
-                );
-              })
-        ],
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
   }
 }
